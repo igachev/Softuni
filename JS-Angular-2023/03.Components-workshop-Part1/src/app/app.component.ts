58db451ec82c128cda56672088e26c4439d2b5eb
igachev <ivophoenix1135@gmail.com>
2023-05-31 13:37:56 +0300

    workshop part 1

---
+++ JS-Angular-2023/03.Components-workshop-Part1/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { ContentService } from './content.service';
+import {  IPost } from './shared/interfaces';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  
+  recentPosts: IPost[] | undefined;
+
+  constructor(private contentService: ContentService) {
+    this.fetchRecentPosts();
+  }
+
+  fetchRecentPosts(): void {
+    this.recentPosts = undefined;
+    this.contentService.loadPosts(5).subscribe((posts) =>
+    this.recentPosts = posts);
+  }
+}
---
+++ JS-Angular-2023/03.Components-workshop-Part1/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { ThemeComponent } from './theme/theme.component';
+import { ContentService } from './content.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ThemeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CoreModule
+  ],
+  providers: [ContentService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
