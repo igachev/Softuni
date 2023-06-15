e32681c9d467b69f9647e20b34bdf63b62165f19
igachev <ivophoenix1135@gmail.com>
2023-06-16 00:42:20 +0300

    added methods in user schema to hash password and compare password

--- JS-Back-End-2023/exam-prep-june-2023/models/User.js
+++ JS-Back-End-2023/exam-prep-june-2023/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 
 const userSchema = new mongoose.Schema({
     username: {
@@ -14,6 +15,16 @@ const userSchema = new mongoose.Schema({
         required:[true,'password is required']
     },
 })
+
+userSchema.pre('save', async function() {
+    const hashedPassword = await bcrypt.hash(this.password,10)
+    this.password = hashedPassword
+})
+
+userSchema.methods.validatePassword = async function(pass) {
+    const checkPassword = await bcrypt.compare(pass,this.password)
+    return checkPassword
+}
 
 const User = mongoose.model('User',userSchema)
 
